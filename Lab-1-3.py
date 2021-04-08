aeac2cf5a2e22fd74216653e75404452f6896952
IrinaSuh <43001581+IrinaSuh@users.noreply.github.com>
2021-04-08 09:56:24 +0300

    Add files via upload

---
+++ Lab-1-3.py
@@ -0,0 +1,14 @@
+import random
+n = int(input("Введите размер списка ="))
+A = []
+for i in range(n):
+    a = random.randint(0,10)
+    A.append(a)
+
+for i in range(n):
+    print(A[i])
+
+k = int(input("Введите номер k="))
+A.pop(k)
+for i in range(n-1):
+    print(A[i])
